1fb57828b7897d987faab6dd9358f07815a51f94
salcedo_r <salcedo_r@f112.lan>
2024-08-21 13:55:42 +0800

    This is already Updated on 1:55PM

--- ActivityGuide1/app.js
+++ ActivityGuide1/app.js
@@ -12,3 +12,46 @@ let x =`Hello ${name}, I see that youre from ${address} and your also pursuing $
 document.getElementById("output1").innerHTML = x
 }
 )
+
+Calculate.addEventListener("click", ()=>{
+let time = document.getElementById("Vehicle_Time").value
+
+let speed = "60"
+
+typeof parseInt(speed)
+typeof parseInt(time)
+
+let distance = speed * time
+
+let message = `ANSWER: The Traveled distance of the Vehicle is ${distance} Miles.`
+
+ document.getElementById("Car_Distance").innerHTML = message
+} 
+)
+
+MPG_Cal.addEventListener("click", ()=>{
+    let Miles = document.getElementById("Miles").value
+    let Galloons = document.getElementById("Gallon").value
+
+    typeof parseInt(Miles)
+    typeof parseInt(Galloons)
+
+    let MPG = Miles / Galloons
+
+    let Calculated_MPG = `ANSWER: Miles-per-Gallon used is ${MPG}.`
+
+    document.getElementById("MPG_ans").innerHTML = Calculated_MPG
+
+}
+)
+
+FtoC.addEventListener("click", ()=>{
+    let cel = document.getElementById("Celsius").value
+    parseInt(cel)
+
+    F = (cel * 9/5) + 32
+
+    let F_Mess = `ANSWER: ${F} Fahrenheit`
+
+    document.getElementById("cel_far").innerHTML = F_Mess
+})
