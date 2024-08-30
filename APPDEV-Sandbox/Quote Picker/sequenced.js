97d89a2da0df183ca9566930d92c007a21749aa9
salcedo_r <salcedo_r@f112.lan>
2024-08-30 13:16:22 +0800

    This has been updated, August 30, 2024

--- APPDEV-Sandbox/Quote Picker/sequenced.js
+++ APPDEV-Sandbox/Quote Picker/sequenced.js
@@ -1,9 +1,9 @@
-const quotes = ["We suffer more in imagination than in reality.", "A gem cannot be polished without friction nor a man perfected without trials", "Dwell on the Beauty of life.", "Everything we hear is an opinion, not a fact.", "The soul becomes dyed with the color of it's thoughts."];
+const quotes = ["1. We suffer more in imagination than in reality.", "2. A gem cannot be polished without friction nor a man perfected without trials.", "3. Dwell on the Beauty of life.", "4. Everything we hear is an opinion, not a fact.", "5. The soul becomes dyed with the color of it's thoughts."];
 const authors = ["-Seneca","-Seneca","-Marcus Aurelius","-Marcus Aurelius","-Marcus Aurelius"]
 const btn = document.getElementById("btn");
 const quote = document.querySelector(".quotes");
 const  author = document.querySelector(".authors");
-let i = 1
+let i = -1
 
 //function for changing the color
 btn.addEventListener("click", ()=>{
@@ -15,7 +15,10 @@ btn.addEventListener("click", ()=>{
 })
 getRandomNumber = () =>{
     i++
-    return i
+    if(i==5){
+       return  i = 0
+    }else{
+    return i}
 
     
 
