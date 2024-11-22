778816597c4c19620d0721033a7b3945f2f7d849
salcedo_r <salcedo_r@f112.lan>
2024-11-22 11:17:09 +0800

    updated

--- react-keyboard-router/src/App.jsx
+++ react-keyboard-router/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './Navbar';
 import Specification from './Specifications';
 import KeyFeatures from './Keyfeatures';
@@ -8,25 +8,22 @@ import Troubleshooting from './Troubleshooting';
 import Maintenance from './Maintenance';
 import './App.css';
 
-
-export default function App(){
-  return(
+export default function App() {
+  return (
     <Router>
       <div className="App">
         <Navbar />
         <div className="content">
-          <Switch>
-            <Route exact path="/" component={() => <h1>Welcome to KB-X1000 Documentation</h1>} />
-            <Route path="/specifications" component={Specification} />
-            <Route path="/key-features" component={KeyFeatures} />
-            <Route path="/setup" component={Setup} />
-            <Route path="/troubleshooting" component={Troubleshooting} />
-            <Route path="/maintenance" component={Maintenance} />
-          </Switch>
+          <Routes>
+            <Route path="/" element={<h1>Welcome to KB-X1000 Documentation</h1>} />
+            <Route path="/specifications" element={<Specification />} />
+            <Route path="/key-features" element={<KeyFeatures />} />
+            <Route path="/setup" element={<Setup />} />
+            <Route path="/troubleshooting" element={<Troubleshooting />} />
+            <Route path="/maintenance" element={<Maintenance />} />
+          </Routes>
         </div>
       </div>
     </Router>
   );
 }
-
-
