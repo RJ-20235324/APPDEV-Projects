fb526b387e8b2a3793ad9212b94499a4fc8078d6
salcedo_r <salcedo_r@f112.lan>
2024-11-27 12:59:06 +0800

    this is updated today nov 27

---
+++ React-MiniProject/src/App.jsx
@@ -0,0 +1,17 @@
+import { Link, Route, Routes } from 'react-router-dom'
+import Navbar from './Components/Navbar'
+
+function App() {
+  return (
+    <>
+      
+      <Navbar/>
+      {/* Routes for the App. */}
+     
+
+      
+    </>
+  )
+}
+
+export default App
---
+++ React-MiniProject/src/Components/Navbar.jsx
@@ -0,0 +1,79 @@
+import React, { useState } from 'react';
+
+export default function Navbar() {
+  const [isSearchActive, setSearchActive] = useState(false);
+
+  const handleSearchClick = () => {
+    setSearchActive(true);
+  };
+
+  const handleCloseClick = () => {
+    setSearchActive(false);
+  };
+
+  return (
+    <nav>
+      <div className="menu">
+        <ul>
+          <li>
+            <img src="/src/assets/K.png" alt="Keystaxx Logo" style={{ width: "40px", height: "40px" }} />
+          </li>
+          <li className={isSearchActive ? "hide-item" : ""}>
+            <a href="#" className="menu-item">
+              Home
+            </a>
+          </li>
+          <li className={isSearchActive ? "hide-item" : ""}>
+            <a href="#" className="menu-item">
+              Products
+            </a>
+          </li>
+          <li className={isSearchActive ? "hide-item" : ""}>
+            <a href="#" className="menu-item">
+              Artisan Keyboards
+            </a>
+          </li>
+          <li className={isSearchActive ? "hide-item" : ""}>
+            <a href="#" className="menu-item">
+              Reviews
+            </a>
+          </li>
+          <li className={isSearchActive ? "hide-item" : ""}>
+            <a href="#" className="menu-item">
+              Company Profile
+            </a>
+          </li>
+          <li className={isSearchActive ? "hide-item" : ""}>
+            <a href="#" className="menu-item">
+              Developer Page
+            </a>
+          </li>
+          <li className={isSearchActive ? "hide-item" : ""}>
+            <a href="#" className="menu-item">
+              Support
+            </a>
+          </li>
+          <li className={isSearchActive ? "hide-item" : ""}>
+            <a href="#" id="search" onClick={handleSearchClick}>
+              <i className="fa fa-search"></i>
+            </a>
+          </li>
+        </ul>
+
+        {isSearchActive && (
+          <div className="search-form active">
+            <form>
+              <input type="text" placeholder="Search Keystaxx..." />
+            </form>
+          </div>
+        )}
+
+        {isSearchActive && (
+          <a href="#" className="close active" onClick={handleCloseClick}>
+            <i className="fa fa-times"></i>
+          </a>
+        )}
+      </div>
+    </nav>
+  );
+}
